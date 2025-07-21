dac75fae80d7a7fa5e4816dbbb7b6cdbb2d3168e
Reece <reeceharris@email.com>
2025-07-21 16:59:22 +0100

    add server hooks to redirect HTTP to HTTPS and specify allowed hosts

--- test/svelte.config.js
+++ test/svelte.config.js
@@ -12,6 +12,19 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
+	},
+	server: {
+		// redirect http to https
+		hooks: {
+			'resolve:before': ({ request }) => {
+				if (request.headers.get('x-forwarded-proto') !== 'https') {
+					return Response.redirect(`https://${request.headers.get('host')}${request.url}`, 301);
+				}
+			}
+		},
+		allowedHosts: [
+			'waf.doodadlabs.org'
+		]
 	}
 };
 
